ca9be55284254386604601903c3ea59c405e7e54
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-08-01 20:26:17 +0300

    building post

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,42 +2,42 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+// Home Routes
 
-//home
 router.get('', async (req, res) => {
-    try {
-      const locals = {
-        title: "Tech Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
-      }
-      let perPage = 6;
-      let page = req.query.page || 1;
-  
-      const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
-      .skip(perPage * page - perPage)
-      .limit(perPage)
-      .exec();
-  
-      // Count is deprecated - please use countDocuments
-      // const count = await Post.count();
-      const count = await Post.countDocuments({});
-      const nextPage = parseInt(page) + 1;
-      const hasNextPage = nextPage <= Math.ceil(count / perPage);
-  
-      res.render('index', { 
-        locals,
-        data,
-        current: page,
-        nextPage: hasNextPage ? nextPage : null,
-        currentRoute: '/'
-      });
-  
-    } catch (error) {
-      console.log(error);
-    }
-  
+  const locals = {
+    title: "NodeJs Blog",
+    description: "Simple Blog created with NodeJs, Express & MongoDb."
+  }
+
+  try {
+    const data = await Post.find();
+    res.render('index', { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
+
+// Post routes
+router.get('/post/:id',async(req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+    // the id in the requesr
+    let slug = req.params.id
+    // Get post by its id
+    const postData = await Post.findById({_id: slug});
+
+    res.render('post',{locals, postData})
+  } catch (error) {
+    console.error(error);
+  }
+  
+})
 
 
 
